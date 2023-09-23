08171c0ae7104e7c1d346e79a5637f2a09e9d25a
Cunegundess <lucascsantana6@gmail.com>
2023-09-22 21:56:34 -0300

    Refatorando as telas

--- BiblioTech/WebAPP/views.py
+++ BiblioTech/WebAPP/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def bibliotechPage(request):
-    return render(request, 'Pages/bibliotech.html')
+    return render(request, 'Pages/initial.html')
 
 def loginPage(request):
     return render(request, 'Pages/login.html')
